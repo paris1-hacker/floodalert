f6496f09e18c5bab9ad719442a71f7e6d3ab6149
ofem patrick <ofempatrick10@gmail.com>
2026-08-27 21:57:43 +0100

    Remove email service and related email verification templates from the project

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,8 +7,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_str
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth import get_user_model
-from accounts.services.email_service import EmailService
-from accounts.services.email_service import EmailService
+# from accounts.services.email_service import EmailService
+# from accounts.services.email_service import EmailService
 
 
 
